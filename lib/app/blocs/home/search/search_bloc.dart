3e79626e223effbc323d28696a9d7b5ea827fb2f
bigbackboom <kodaik@me.com>
2020-05-24 14:53:37 +0900

    Androidのロケーション周りを設定

--- lib/app/blocs/home/search/search_bloc.dart
+++ lib/app/blocs/home/search/search_bloc.dart
@@ -1,17 +1,21 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:letme_app/app/blocs/base_bloc.dart';
 import 'package:letme_app/app/repositories/search_repository.dart';
 import 'package:letme_app/app/resources/models/home/search_main.dart';
 import 'package:rxdart/rxdart.dart';
 
-class SearchBloc implements BaseBloc{
-
+class SearchBloc implements BaseBloc {
   final _repository = SearchRepository();
   final _result = BehaviorSubject<SearchMainContents>();
+  final _position = BehaviorSubject<Position>();
   Stream<SearchMainContents> searchMain;
+  Stream<Position> position;
 
-  SearchBloc(){
+  SearchBloc() {
     searchMain = _result.stream;
+    position = _position.stream;
     _fetchSearchMain();
+    getCurrentLocation();
   }
 
   void _fetchSearchMain() async {
@@ -20,8 +24,14 @@ class SearchBloc implements BaseBloc{
     });
   }
 
-  void dispose() {
-    _result.close();
+  void getCurrentLocation() async {
+    await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((value) => _position.sink.add(value));
   }
 
+  void dispose() {
+    _result.close();
+    _position.close();
+  }
 }
